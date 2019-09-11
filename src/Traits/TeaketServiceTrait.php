b7ef09d7de1c5fc7862952c892d241435cf15d95
Oluwasegun Kesington <iamkesington@gmail.com>
2019-09-11 04:45:29 +0100

    Import Eloquent Model

--- src/Traits/TeaketServiceTrait.php
+++ src/Traits/TeaketServiceTrait.php
@@ -6,6 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Donkovah\Teaket\Model\Teaket;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * This trait assumes you make use of Laravel's 
